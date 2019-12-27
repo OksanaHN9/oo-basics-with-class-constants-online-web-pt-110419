92c961b23976bab8bd02e86f5fc72d17566295f4
Oksana Nichols <oksanahnichols@yahoo.com>
2019-12-27 03:39:21 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,8 @@
 class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
-  
+  describe 'GENRES' do 
+    it 'keeps track of all genres' do
   GENRES = ["Thriller", "Science Fiction", "Romance"]
 
   def initialize(title)
